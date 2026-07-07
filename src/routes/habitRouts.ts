b92c4e7e22f5b3e27c1386f4acacb83bb9c79ed1
swagatpadhi <litan3104@gmail.com>
2026-07-07 16:37:47 +0530

    restart

---
+++ src/routes/habitRouts.ts
@@ -0,0 +1,19 @@
+import {Router} from 'express'
+
+const router = Router()
+
+
+router.get('/',(req,res)=>{
+    res.json({message:"habits"})
+
+})
+
+router.get('/:id',(req,res)=>{
+    res.json({message:"one habits"})
+
+})
+
+router.post('/',(req,res)=>{
+    res.json({message:"habits created"})
+
+})
