8871f461d0bbe2aa1d1523272c5f471d2cf6e652
swagatpadhi <litan3104@gmail.com>
2026-07-10 22:55:06 +0530

    restart

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -5,3 +5,23 @@ const router = Router()
 router.get('/',(req,res)=>{
     res.json("message:all users")
 })
+router.get('/:id',(req,res)=>{
+    res.json({message:"one user"})
+
+})
+
+router.post('/',(req,res)=>{
+    res.json({message:"user created"})
+
+})
+router.put('/:id',(req,res)=>{
+    res.json({message:"user updated"})
+
+})
+
+router.delete('/:id',(req,res)=>{
+    res.json({message:"user deleted"})
+
+})
+
+export default router
