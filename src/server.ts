2c9ed6e5091d181f23e7980140d014eb3e962a36
swagatpadhi <litan3104@gmail.com>
2026-07-17 18:23:35 +0530

    restart

--- src/server.ts
+++ src/server.ts
@@ -5,10 +5,15 @@ import habitRoutes from './routes/habitRouts'
 import morgan from 'morgan'
 import cors from 'cors'
 import helmet from 'helmet'
+import {isTesting} from '../env'
 
 
 const app = express()
 app.use(cors())
+app.use(helmet())
+app.use(morgan('dev',{
+    skip:()=> isTesting()
+}))
 
 app.get('/health',(req,res)=>{
     // res.json({message:'hello'}).status(200)
