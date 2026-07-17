0e8df623594857d16235ccb602c640deb4be9a91
swagatpadhi <litan3104@gmail.com>
2026-07-17 18:19:15 +0530

    added

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,13 @@ import express from 'express'
 import authRoutes from './routes/authRoutes'
 import userRoutes from './routes/userRoutes'
 import habitRoutes from './routes/habitRouts'
+import morgan from 'morgan'
+import cors from 'cors'
+import helmet from 'helmet'
+
 
 const app = express()
+app.use(cors())
 
 app.get('/health',(req,res)=>{
     // res.json({message:'hello'}).status(200)
